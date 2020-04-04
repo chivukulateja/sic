72a4ef02f3a03f3e9686d5412901cdabb0428efc
Chivukula Teja <chivukula.t7@gmail.com>
2020-04-04 23:52:05 +0530

    form images

--- server.php
+++ server.php
@@ -7,7 +7,7 @@ $prob = $_REQUEST['prob'];
 if ( empty( $name ) || empty( $work ) || empty( $prob ) ) {
     echo 'Please fill All Fields';
 } else {
-    mail( 'chivukula.t7@gmail.com', 'Teja', $prob, "From $name <$work>" );
+    mail( 'chivukula.t7@gmail.com', 'Teja', $prob, "From $name <chivukulateja@outlook.com>" );
     echo "<script type='tect/javascript'>alert('success');
     window.history.log( -1 );</script>";
 
