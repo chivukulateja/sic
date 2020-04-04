687f4d9bc9bfd75c1f042fa6fa3b0c31f128e43b
Chivukula Teja <chivukula.t7@gmail.com>
2020-04-05 00:06:21 +0530

    form images

--- server.php
+++ server.php
@@ -1,15 +1,20 @@
 <?php
+if ( isset( $_POST['submit'] ) ) {
+    $name = $_POST['name'];
+    $email = 'chivukula.t7@gmail.com';
+    $work = $_POST['work'];
+    $msg = $_POST['prob'];
 
-$name = $_REQUEST['name'];
-$work = $_REQUEST['work'];
-$prob = $_REQUEST['prob'];
+    $to = 'chivukula.t7@mail.com';
+    // Receiver Email ID, Replace with your email ID
+    $subject = 'Form Submission';
+    $message = 'Name :'.$name.'\n'.'Phone :'.$work.'\n'.'Wrote the following :'.'\n\n'.$msg;
+    $headers = 'From: '.$email;
 
-if ( empty( $name ) || empty( $work ) || empty( $prob ) ) {
-    echo 'Please fill All Fields';
-} else {
-    mail( 'chivukula.t7@gmail.com', 'Teja', $prob, "From $name <chivukulateja@outlook.com>" );
-    echo "<script type='tect/javascript'>alert('success');
-    window.history.log( -1 );</script>";
-
+    if ( mail( $to, $subject, $message, $headers ) ) {
+        echo '<h1>Sent Successfully! Thank you'.' '.$name.', We will contact you shortly!</h1>';
+    } else {
+        echo 'Something went wrong!';
+    }
 }
 ?>
